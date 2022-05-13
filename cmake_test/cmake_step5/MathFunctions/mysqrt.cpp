eba8d15e4bae499c4ee12dfa4d5d4980475dbd22
wsltsg1988 <573354178@qq.com>
2022-05-13 18:23:36 +0800

    Add files via upload

---
+++ cmake_test/cmake_step5/MathFunctions/mysqrt.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+// a hack square root calculation using simple operations
+double mysqrt(double x)
+{
+  if (x <= 0) {
+    return 0;
+  }
+
+  double result = x;
+  #if defined(HAVE_LOG) && defined(HAVE_EXP)
+     result = exp(log(x) * 0.5);
+     std::cout << "Computing sqrt of " << x << " to be " << result
+            << " using log and exp" << std::endl;
+  #else
+    // do ten iterations
+    for (int i = 0; i < 10; ++i) {
+      if (result <= 0) {
+        result = 0.1;
+      }
+      double delta = x - (result * result);
+      result = result + 0.5 * delta / result;
+      std::cout << "Computing sqrt of " << x << " to be " << result << " using  mysqrt" << std::endl;
+    }
+   #endif 
+
+  return result;
+}
---
+++ cmake_test/cmake_step9/MathFunctions/mysqrt.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "MathFunctions.h"
+
+// a hack square root calculation using simple operations
+#include "Table.h"  //自动生成
+
+namespace mathfunctions {
+namespace detail {
+double mysqrt(double x)
+{
+  if (x <= 0) {
+    return 0;
+  }
+
+  double result = x;
+   if (x >= 1 && x < 10) {
+    std::cout << "Use the table to help find an initial value " << std::endl;
+    result = sqrtTable[static_cast<int>(x)];
+    std::cout << "Computing sqrt of " << x << " to be " << result << " using  mysqrt" << std::endl; 
+    return result;
+  }
+
+  // do ten iterations
+  for (int i = 0; i < 10; ++i) {
+    if (result <= 0) {
+      result = 0.1;
+    }
+    double delta = x - (result * result);
+    result = result + 0.5 * delta / result;
+    std::cout << "Computing sqrt of " << x << " to be " << result << " using  mysqrt" << std::endl;
+  }
+  return result;
+}
+}
+}
